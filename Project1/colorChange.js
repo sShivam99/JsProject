0f9a8a8c0d2b479cfdace2cfa755b80591f775af
sShivam99 <iamshivamsinha99@gmail.com>
2024-12-21 20:17:12 +0000

    Project 1

---
+++ Project1/colorChange.js
@@ -0,0 +1,15 @@
+const button=document.querySelectorAll('.button');
+const body=document.querySelector('body');
+
+button.forEach((button)=>{
+  button.addEventListener('click',
+  (e)=>{
+    console.log(typeof(e.target.id));
+    const color=e.target.id;
+
+    changeColor(color);
+    function changeColor(color){
+      body.style.backgroundColor=color;
+    }
+  })
+})
