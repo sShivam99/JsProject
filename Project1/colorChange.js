3f02442fd7b808992294986a2fb6015d69c24b83
sShivam99 <iamshivamsinha99@gmail.com>
2024-12-22 02:01:37 +0530

    Update colorChange.js

--- Project1/colorChange.js
+++ Project1/colorChange.js
@@ -4,9 +4,7 @@ const body=document.querySelector('body');
 button.forEach((button)=>{
   button.addEventListener('click',
   (e)=>{
-    console.log(typeof(e.target.id));
     const color=e.target.id;
-
     changeColor(color);
     function changeColor(color){
       body.style.backgroundColor=color;
